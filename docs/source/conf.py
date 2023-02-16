87193765e77cd4734265e51977f1a19a5c44d32f
Nace Kranjc <nace.kranjc@gmail.com>
2023-02-16 22:01:58 +0000

    Readthedocs definitions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,14 +27,15 @@ author = "Nace Kranjc"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "readthedocs_ext.readthedocs",
     "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
     "sphinx.ext.autodoc",
-    "numpydoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.githubpages",
     "sphinx.ext.intersphinx",
     "sphinx.ext.extlinks",
+    "numpydoc",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -52,7 +53,7 @@ autodoc_mock_imports = ["pyfaidx", "allel", "pyranges"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
